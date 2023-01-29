c2fb0c0f2c5144c0a84651ef8f80d9965377b57f
Guojijian <qq1943889324@163.com>
2023-01-29 10:33:04 +0800

    hot-fix commit2

--- src/test/java/Test.java
+++ src/test/java/Test.java
@@ -4,6 +4,6 @@ public class Test {
         System.out.println("hello git 2");
         System.out.println("hello git 3");
         System.out.println("hello git 4");
-        System.out.println("hello git hot-fix");
+        System.out.println("hello git hot-fix2");
     }
 }
